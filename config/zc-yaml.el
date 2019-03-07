ab19a8d12f051d6d8033824cbda0325f7da1c0cf
Frederick Cai <frederick.cai@gmail.com>
2019-03-08 10:53:43 +1300

    yaml: highlight variable names

--- config/zc-yaml.el
+++ config/zc-yaml.el
@@ -5,7 +5,14 @@
   :straight t
   :defer t
   :mode ("\\.yaml\\'" "\\.yml\\'")
+
+  :preface
+  (defun zc-yaml/override-font-lock-faces ()
+    (face-remap-add-relative 'font-lock-variable-name-face
+                             :foreground (doom-color 'violet)))
+
   :hook ((yaml-mode . company-mode)
-         (yaml-mode . flycheck-mode-on-safe)))
+         (yaml-mode . flycheck-mode-on-safe)
+         (yaml-mode . zc-yaml/override-font-lock-faces)))
 
 (provide 'zc-yaml)
