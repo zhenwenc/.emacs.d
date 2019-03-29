782c9389553aa56ab868fab51ee3d980b68e5d23
Frederick Cai <frederick.cai@gmail.com>
2019-03-30 11:08:59 +1300

    eval: fix compilation error regex for jest

--- config/zc-eval.el
+++ config/zc-eval.el
@@ -1,6 +1,26 @@
 (eval-when-compile
   (require 'use-package))
 
+(require 's)
+(require 'dash)
+
+
+
+(defconst zc-eval/jest-error-rx
+  (rx bol "at" (+? nonl)
+      "("
+      (group (+? nonl)) ":" ;; filename
+      (group (+ digit)) ":" ;; line
+      (group (+ digit))     ;; column
+      ")" eol))
+
+(-let* ((str "at Object.it (core/__tests__/Option.spec.ts:8:38)")
+        ((whole file line column) (s-match zc-eval/jest-error-rx str)))
+  (cl-assert (equal whole str))
+  (cl-assert (equal file   "core/__tests__/Option.spec.ts"))
+  (cl-assert (equal line   "8"))
+  (cl-assert (equal column "38")))
+
 
 
 (use-package compile
@@ -11,19 +31,28 @@
   (:states 'motion :keymaps 'compilation-mode-map
            "h" #'evil-backward-char)
 
+  (:states 'normal :keymaps 'compilation-mode-map
+           "g" #'recompile)
+
   :preface
-  (defun zc-core/colorize-compilation-buffer ()
+  (defun zc-eval/colorize-compilation-buffer ()
     (unless (derived-mode-p 'rg-mode)
       (with-silent-modifications
         (ansi-color-apply-on-region compilation-filter-start (point)))))
 
-  :hook (compilation-filter . zc-core/colorize-compilation-buffer)
+  :hook (compilation-filter . zc-eval/colorize-compilation-buffer)
 
   :init
   (setq compilation-environment '("TERM=screen-256color")
         compilation-always-kill t
         compilation-ask-about-save nil
-        compilation-scroll-output 'first-error))
+        compilation-scroll-output 'first-error)
+
+  :config
+  (progn
+    (setf (alist-get 'jest compilation-error-regexp-alist-alist)
+          (list zc-eval/jest-error-rx 1 2 3))
+    (add-to-list 'compilation-error-regexp-alist 'jest)))
 
 (use-package quickrun
   :straight t
