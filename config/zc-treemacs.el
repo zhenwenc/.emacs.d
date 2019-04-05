984a97599cfe0a95636fac75cddcd75de5f9e24b
Frederick Cai <frederick.cai@gmail.com>
2019-04-05 18:38:32 +1300

    treemacs: refine keybindings

--- config/zc-treemacs.el
+++ config/zc-treemacs.el
@@ -1,6 +1,8 @@
 (eval-when-compile
   (require 'use-package))
 
+(require 'general)
+
 
 
 (use-package treemacs
@@ -16,49 +18,53 @@
           (-contains? (projectile-ignored-directories) file))))
 
   :config
-  (progn
-    (setq
-     ;; Path to the file treemacs uses to persist its state
-     treemacs-persist-file (f-join paths-cache-dir "treemacs-persist")
-     ;; Follow the currently selected file
-     treemacs-follow-after-init t
-     ;; Prevents treemacs from being selected with `other-window`
-     treemacs-is-never-other-window t)
+  (setq
+   ;; Path to the file treemacs uses to persist its state
+   treemacs-persist-file (f-join paths-cache-dir "treemacs-persist")
+   ;; Follow the currently selected file
+   treemacs-follow-after-init t
+   ;; Prevents treemacs from being selected with `other-window`
+   treemacs-is-never-other-window t)
 
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-git-mode 'simple)
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+  (treemacs-git-mode 'simple)
 
-    ;; Hide noisy files and directories
-    (add-to-list 'treemacs-pre-file-insert-predicates
-                 #'zc-treemacs/is-file-ignored?)
+  ;; Hide noisy files and directories
+  (add-to-list 'treemacs-pre-file-insert-predicates
+               #'zc-treemacs/is-file-ignored?)
 
-    ;; Disable the indicator next to open files--hl-line is sufficient
-    (treemacs-fringe-indicator-mode nil)
+  ;; Disable the indicator next to open files--hl-line is sufficient
+  (treemacs-fringe-indicator-mode nil)
 
-    ;; Define custom key bindings
-    (zc-hydra/major-mode-define treemacs-mode
-      ("Basic"
-       (("?" treemacs--helpful-hydra "helpful"))
+  ;; Define custom key bindings
+  (zc-hydra/major-mode-define treemacs-mode
+    ("Basic"
+     (("?" treemacs--helpful-hydra "helpful"))
 
-       "Project"
-       (("pp" treemacs-projectile "add project")
-        ("pd" treemacs-remove-project-from-workspace "remove project")
-        ("pr" treemacs-rename-project "rename project")
-        ("pc" treemacs-collapse-project "collapse project")
-        ("pC" treemacs-collapse-all-projects "collapse all projects"))
+     "Project"
+     (("pp" treemacs-projectile "add project")
+      ("pd" treemacs-remove-project-from-workspace "remove project")
+      ("pr" treemacs-rename-project "rename project")
+      ("pc" treemacs-collapse-project "collapse project")
+      ("pC" treemacs-collapse-all-projects "collapse all projects"))
 
-       "Workspace"
-       (("wc" treemacs-create-workspace "create workspace")
-        ("wo" treemacs-switch-workspace "select workspace")
-        ("wD" treemacs-remove-workspace "remove workspace"))))
-    ))
+     "Workspace"
+     (("wc" treemacs-create-workspace "create workspace")
+      ("wo" treemacs-switch-workspace "select workspace")
+      ("wD" treemacs-remove-workspace "remove workspace")))))
 
 (use-package treemacs-evil
   :straight t
   :after (:and treemacs evil)
   :config
-  (setq evil-treemacs-state-cursor '("SkyBlue" box)))
+  (setq evil-treemacs-state-cursor '("SkyBlue" box))
+
+  (general-define-key :keymaps 'treemacs-mode-map :states 'treemacs
+                      "h" #'treemacs-collapse-parent-node
+                      "l" #'treemacs-TAB-action
+                      "H" #'treemacs-root-up
+                      "L" #'treemacs-root-down))
 
 (use-package treemacs-projectile
   :straight t
