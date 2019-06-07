ba8c094da5baba5ecf1a7868663fe0e184784336
Frederick Cai <frederick.cai@gmail.com>
2019-06-08 09:02:16 +1200

    company: switch to company-box

--- config/zc-company.el
+++ config/zc-company.el
@@ -25,7 +25,7 @@
    "S-<return>"                #'company-complete)
 
   (:keymaps 'comint-mode-map
-            [remap indent-for-tab-command] #'company-manual-begin)
+   [remap indent-for-tab-command] #'company-manual-begin)
 
   :preface
   (defun zc-company/setup ()
@@ -58,14 +58,21 @@
         company-require-match nil
         company-minimum-prefix-length 2
 
-        company-dabbrev-downcase nil
-        company-dabbrev-ignore-case nil
-        company-dabbrev-code-ignore-case nil
-        company-tooltip-limit 10
+        company-tooltip-limit 12
         company-tooltip-align-annotations t
 
-        company-frontends '(company-pseudo-tooltip-unless-just-one-frontend
-                            company-preview-frontend
+        ;; Only search the current buffer to get suggestions
+        ;; for `company-dabbrev'. This prevents company from
+        ;; causing lag when there are many buffers open.
+        company-dabbrev-other-buffers nil
+        company-dabbrev-code-ignore-case nil
+        company-dabbrev-ignore-case nil
+        company-dabbrev-downcase nil
+
+        ;; Always display suggestions in the tooltip, even if
+        ;; there is only one. Also display metadata in the echo
+        ;; area. (But this conflicts with ElDoc)
+        company-frontends '(company-pseudo-tooltip-frontend
                             company-echo-metadata-frontend)
         company-backends '(company-capf)
         company-transformers '(company-sort-by-occurrence)))
@@ -73,12 +80,53 @@
 (use-package company-dabbrev
   :after company)
 
-(use-package company-statistics
+;; Provides intelligent sorting and filtering for candidates.
+(use-package company-prescient
   :straight t
   :after company
+  :hook (company-mode . company-prescient-mode)
   :config
-  (setq company-statistics-file (concat paths-cache-dir "/company-statistics"))
-  (company-statistics-mode +1))
+  (setq prescient-save-file (concat paths-cache-dir "/prescient-save.el"))
+  (prescient-persist-mode +1))
+
+(use-package company-box
+  :straight t
+  :after company
+  :hook (company-mode . company-box-mode)
+  :config
+  (setq company-box-show-single-candidate t
+        company-box-backends-colors nil
+        company-box-max-candidates 50
+        company-box-icons-alist 'company-box-icons-all-the-icons)
+
+  (setq company-box-icons-all-the-icons
+        `((Unknown       . ,(all-the-icons-material "find_in_page" :height 0.7  :v-adjust -0.15))
+          (Text          . ,(all-the-icons-faicon   "book" :height 0.68 :v-adjust -0.15))
+          (Method        . ,(all-the-icons-faicon   "cube" :height 0.7  :v-adjust -0.05 :face 'font-lock-constant-face))
+          (Function      . ,(all-the-icons-faicon   "cube" :height 0.7  :v-adjust -0.05 :face 'font-lock-constant-face))
+          (Constructor   . ,(all-the-icons-faicon   "cube" :height 0.7  :v-adjust -0.05 :face 'font-lock-constant-face))
+          (Field         . ,(all-the-icons-faicon   "tags" :height 0.65 :v-adjust -0.15 :face 'font-lock-warning-face))
+          (Variable      . ,(all-the-icons-faicon   "tag" :height 0.7  :v-adjust -0.05 :face 'font-lock-warning-face))
+          (Class         . ,(all-the-icons-faicon   "clone" :height 0.65 :v-adjust 0.01 :face 'font-lock-constant-face))
+          (Interface     . ,(all-the-icons-faicon   "clone" :height 0.65 :v-adjust 0.01))
+          (Module        . ,(all-the-icons-octicon  "package" :height 0.7  :v-adjust -0.15))
+          (Property      . ,(all-the-icons-octicon  "package" :height 0.7  :v-adjust -0.05 :face 'font-lock-warning-face))
+          (Unit          . ,(all-the-icons-material "settings_system_daydream" :height 0.7 :v-adjust -0.15))
+          (Value         . ,(all-the-icons-material "format_align_right" :height 0.7 :v-adjust -0.15 :face 'font-lock-constant-face))
+          (Enum          . ,(all-the-icons-material "storage" :height 0.7 :v-adjust -0.15 :face 'all-the-icons-orange))
+          (Keyword       . ,(all-the-icons-material "filter_center_focus" :height 0.7 :v-adjust -0.15))
+          (Snippet       . ,(all-the-icons-faicon   "code" :height 0.7 :v-adjust 0.02 :face 'font-lock-variable-name-face))
+          (Color         . ,(all-the-icons-material "palette" :height 0.7 :v-adjust -0.15))
+          (File          . ,(all-the-icons-faicon   "file-o" :height 0.7 :v-adjust -0.05))
+          (Reference     . ,(all-the-icons-material "collections_bookmark" :height 0.7 :v-adjust -0.15))
+          (Folder        . ,(all-the-icons-octicon  "file-directory" :height 0.7 :v-adjust -0.05))
+          (EnumMember    . ,(all-the-icons-material "format_align_right" :height 0.7 :v-adjust -0.15 :face 'all-the-icons-blueb))
+          (Constant      . ,(all-the-icons-faicon   "tag" :height 0.7 :v-adjust -0.05))
+          (Struct        . ,(all-the-icons-faicon   "clone" :height 0.65 :v-adjust 0.01 :face 'font-lock-constant-face))
+          (Event         . ,(all-the-icons-faicon   "bolt" :height 0.7 :v-adjust -0.05 :face 'all-the-icons-orange))
+          (Operator      . ,(all-the-icons-fileicon "typedoc" :height 0.65 :v-adjust 0.05))
+          (TypeParameter . ,(all-the-icons-faicon   "hashtag" :height 0.65 :v-adjust 0.07 :face 'font-lock-const-face))
+          (Template      . ,(all-the-icons-faicon   "code" :height 0.7 :v-adjust 0.02 :face 'font-lock-variable-name-face)))))
 
 
 
