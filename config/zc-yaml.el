31f2d13d54d0ea28a949aeb6404c97c42ecbcf90
Frederick Cai <frederick.cai@mattr.global>
2021-05-12 08:54:21 +1200

    yaml: minor cleanup

--- config/zc-yaml.el
+++ config/zc-yaml.el
@@ -1,18 +1,22 @@
 (eval-when-compile
   (require 'use-package))
 
+
+
 (use-package yaml-mode
   :straight t
   :defer t
   :mode ("\\.yaml\\'" "\\.yml\\'")
 
-  :preface
-  (defun zc-yaml/override-font-lock-faces ()
-    (face-remap-add-relative 'font-lock-variable-name-face
-                             :foreground (doom-color 'violet)))
-
   :hook ((yaml-mode . company-mode)
          (yaml-mode . flycheck-mode-on-safe)
-         (yaml-mode . zc-yaml/override-font-lock-faces)))
+         (yaml-mode . zc-yaml/override-font-lock-faces))
+
+  :init
+  (defun zc-yaml/override-font-lock-faces ()
+    (face-remap-add-relative 'font-lock-variable-name-face
+                             :foreground (doom-color 'violet))))
+
+
 
 (provide 'zc-yaml)
