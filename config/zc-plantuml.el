a14cd17119124372a7d505ee07d16dd87b6be9fe
Frederick Cai <frederick.cai@mattr.global>
2021-05-12 08:55:16 +1200

    plantuml: improve preview feature

--- config/zc-plantuml.el
+++ config/zc-plantuml.el
@@ -75,7 +75,8 @@
   ;;   Use `:output' to specify output type: txt, svg, or png.
   ;;
   (defun zc-plantuml/org-babel-execute (orig-fn body params)
-    (if (s-equals? "none" (cdr (assq :results params)))
+    (if (and (null org-export-current-backend) ;; not in `org-export-as' call
+             (not (s-equals? "no" (cdr (assq :preview params)))))
         (let* ((output-type          (cdr (assq :output params)))
                (plantuml-output-type (or output-type "png"))
                (full-body (org-babel-plantuml-make-body body params)))
