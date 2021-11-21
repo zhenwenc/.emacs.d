5e17e70920c32f3cfd5032c33fbc088053ebecfc
Frederick Cai <frederick.cai@gmail.com>
2021-11-22 08:34:58 +1300

    kotlin: remove android mode

--- config/zc-kotlin.el
+++ config/zc-kotlin.el
@@ -10,15 +10,7 @@
 ;; Major mode for Groovy and Grails files.
 (use-package groovy-mode
   :straight t
-  :defer t
   :mode "\\.gradle\\'")
-
-;; Provides support for running Android SDK subprocesses like the
-;; emulator, logcat, ddms and ant.
-(use-package android-mode
-  :straight t
-  :defer t
-  :mode "build\\.gradle\\'")
 
 
 
