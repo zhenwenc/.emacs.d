2b2bb739f34991509eaecd933ec459bed9975427
Frederick Cai <frederick.cai@mattr.global>
2023-08-26 08:39:50 +1200

    chore!: prepare to migrate to doom-emacs

--- init.el
+++ init.el
@@ -1,178 +1,195 @@
-;;; init.el --- Startup file for Emacs.  -*- lexical-binding: t; -*-
+;;; init.el -*- lexical-binding: t; -*-
+
+;; This file controls what Doom modules are enabled and what order they load
+;; in. Remember to run 'doom sync' after modifying it!
+
+;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
+
+;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
+;;      'C-c c k' for non-vim users) to view its documentation. This works on
+;;      flags as well (those symbols that start with a plus).
 ;;
-;; Copyright (C) 2018 Frederick Cai
-;;
-;; Author: Frederick Cai <frederick.cai@gmail.com>
-;;
-;;; Commentary:
+;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
+;;      directory (for easy access to its source code).
 
-;; This emacs setting is adapted from Chris Barrett's config here:
-;; https://github.com/chrisbarrett/.emacs.d
+(doom! :input
+       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
+       ;;chinese
+       ;;japanese
+       ;;layout            ; auie,ctsrnm is the superior home row
 
-;; Install libgccjit if necessary:
-;;
-;;   brew reinstall gcc libgccjit
-;;
-;; Install Emacs on MacOS:
-;; https://github.com/railwaycat/homebrew-emacsmacport
-;;
-;;   brew install emacs-mac --with-emacs-sexy-icon
-;;
-;; https://github.com/d12frosted/homebrew-emacs-plus
-;;
-;;   brew install emacs-plus --with-modern-sexy-v2-icon
+       :completion
+       company           ; the ultimate code completion backend
+       ;;helm              ; the *other* search engine for love and life
+       ;;ido               ; the other *other* search engine...
+       ;;ivy               ; a search engine for love and life
+       vertico           ; the search engine of the future
 
-(when (version< emacs-version "26")
-  (error "This requires Emacs 26 and above!"))
+       :ui
+       ;;deft              ; notational velocity for Emacs
+       doom              ; what makes DOOM look the way it does
+       doom-dashboard    ; a nifty splash screen for Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;(emoji +unicode)  ; 🙂
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       ;;hydra
+       ;;indent-guides     ; highlighted indent columns
+       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ;;minimap           ; show a map of the code on the side
+       modeline          ; snazzy, Atom-inspired modeline, plus API
+       ;;nav-flash         ; blink cursor line after big motions
+       ;;neotree           ; a project drawer, like NERDTree for vim
+       ophints           ; highlight the region an operation acts on
+       (popup +defaults)   ; tame sudden yet inevitable temporary windows
+       ;;tabs              ; a tab bar for Emacs
+       ;;treemacs          ; a project drawer, like neotree but cooler
+       ;;unicode           ; extended unicode support for various languages
+       (vc-gutter +pretty) ; vcs diff in the fringe
+       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       ;;window-select     ; visually switch windows
+       workspaces        ; tab emulation, persistence & separate workspaces
+       ;;zen               ; distraction-free coding or writing
 
-(defconst emacs-start-time (current-time))
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
+       ;;(format +onsave)  ; automated prettiness
+       ;;god               ; run Emacs commands without modifier keys
+       ;;lispy             ; vim for lisp, for people who don't like vim
+       ;;multiple-cursors  ; editing in many places at once
+       ;;objed             ; text object editing for the innocent
+       ;;parinfer          ; turn lisp into python, sort of
+       ;;rotate-text       ; cycle region at point between text candidates
+       snippets          ; my elves. They type so I don't have to
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
-(unless noninteractive
-  (message "Loading %s..." load-file-name))
+       :emacs
+       dired             ; making dired pretty [functional]
+       electric          ; smarter, keyword-based electric-indent
+       ;;ibuffer         ; interactive buffer management
+       undo              ; persistent, smarter undo for your inevitable mistakes
+       vc                ; version-control and Emacs, sitting in a tree
 
-
+       :term
+       ;;eshell            ; the elisp shell that works everywhere
+       ;;shell             ; simple shell REPL for Emacs
+       ;;term              ; basic terminal emulator for Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
-(defun zc/increase-gc-limit ()
-  "Increase garbage collection limit to maximum value."
-  (setq gc-cons-threshold most-positive-fixnum))
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       ;;grammar           ; tasing grammar mistake every you make
 
-(defun zc/reset-gc-limit ()
-  "Restore to defalut value, 16mb."
-  (setq gc-cons-threshold (* 16 1024 1024)))
+       :tools
+       ;;ansible
+       ;;biblio            ; Writes a PhD for you (citation needed)
+       ;;collab            ; buffers with friends
+       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       ;;direnv
+       ;;docker
+       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       ;;ein               ; tame Jupyter notebooks with emacs
+       (eval +overlay)     ; run code, run (also, repls)
+       ;;gist              ; interacting with github gists
+       lookup              ; navigate your code and its documentation
+       ;;lsp               ; M-x vscode
+       magit             ; a git porcelain for Emacs
+       ;;make              ; run make tasks from Emacs
+       ;;pass              ; password manager for nerds
+       ;;pdf               ; pdf enhancements
+       ;;prodigy           ; FIXME managing external services & code builders
+       ;;rgb               ; creating color strings
+       ;;taskrunner        ; taskrunner for all your projects
+       ;;terraform         ; infrastructure as code
+       ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       ;;upload            ; map local to remote projects via ssh/ftp
 
-;; Defer GC further back to speed up startup
-(zc/increase-gc-limit)
-(add-hook 'emacs-startup-hook #'zc/reset-gc-limit)
+       :os
+       (:if IS-MAC macos)  ; improve compatibility with macOS
+       ;;tty               ; improve the terminal Emacs experience
 
-
-;; Bootstrap straight.el package manager.
+       :lang
+       ;;agda              ; types of types of types of types...
+       ;;beancount         ; mind the GAAP
+       ;;(cc +lsp)         ; C > C++ == 1
+       ;;clojure           ; java with a lisp
+       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       ;;coq               ; proofs-as-programs
+       ;;crystal           ; ruby at the speed of c
+       ;;csharp            ; unity, .NET, and mono shenanigans
+       ;;data              ; config/data formats
+       ;;(dart +flutter)   ; paint ui and not much else
+       ;;dhall
+       ;;elixir            ; erlang done right
+       ;;elm               ; care for a cup of TEA?
+       emacs-lisp        ; drown in parentheses
+       ;;erlang            ; an elegant language for a more civilized age
+       ;;ess               ; emacs speaks statistics
+       ;;factor
+       ;;faust             ; dsp, but you get to keep your soul
+       ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
+       ;;fsharp            ; ML stands for Microsoft's Language
+       ;;fstar             ; (dependent) types and (monadic) effects and Z3
+       ;;gdscript          ; the language you waited for
+       ;;(go +lsp)         ; the hipster dialect
+       ;;(graphql +lsp)    ; Give queries a REST
+       ;;(haskell +lsp)    ; a language that's lazier than I am
+       ;;hy                ; readability of scheme w/ speed of python
+       ;;idris             ; a language you can depend on
+       ;;json              ; At least it ain't XML
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
+       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       ;;julia             ; a better, faster MATLAB
+       ;;kotlin            ; a better, slicker Java(Script)
+       ;;latex             ; writing papers in Emacs has never been so fun
+       ;;lean              ; for folks with too much to prove
+       ;;ledger            ; be audit you can be
+       ;;lua               ; one-based indices? one-based indices
+       markdown          ; writing docs for people to ignore
+       ;;nim               ; python + lisp at the speed of c
+       ;;nix               ; I hereby declare "nix geht mehr!"
+       ;;ocaml             ; an objective camel
+       org               ; organize your plain life in plain text
+       ;;php               ; perl's insecure younger brother
+       ;;plantuml          ; diagrams for confusing people more
+       ;;purescript        ; javascript, but functional
+       ;;python            ; beautiful is better than ugly
+       ;;qt                ; the 'cutest' gui framework ever
+       ;;racket            ; a DSL for DSLs
+       ;;raku              ; the artist formerly known as perl6
+       ;;rest              ; Emacs as a REST client
+       ;;rst               ; ReST in peace
+       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;scala             ; java, but good
+       ;;(scheme +guile)   ; a fully conniving family of lisps
+       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       ;;sml
+       ;;solidity          ; do you need a blockchain? No.
+       ;;swift             ; who asked for emoji variables?
+       ;;terra             ; Earth and Moon in alignment for performance.
+       ;;web               ; the tubes
+       ;;yaml              ; JSON, but readable
+       ;;zig               ; C, but simpler
 
-(defvar bootstrap-version)
-(let ((bootstrap-version 5)
-      (bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory)))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
+       :email
+       ;;(mu4e +org +gmail)
+       ;;notmuch
+       ;;(wanderlust +gmail)
 
-  ;; Improve bootstrap speed. The cache wile may grow quite
-  ;; large overtime, if so run `straight-prune-build'.
-  (with-no-warnings
-    (setq straight-cache-autoloads t)
-    (setq straight-check-for-modifications 'live)
-    (setq straight-disable-native-compile nil))
+       :app
+       ;;calendar
+       ;;emms
+       ;;everywhere        ; *leave* Emacs!? You must be joking
+       ;;irc               ; how neckbeards socialize
+       ;;(rss +org)        ; emacs as an RSS reader
+       ;;twitter           ; twitter client https://twitter.com/vnought
 
-  (load bootstrap-file nil 'nomessage))
-
-
-;; Install basic packages
-
-(straight-use-package 'dash)
-(straight-use-package 'f)
-(straight-use-package 's)
-(straight-use-package 'lv)
-(straight-use-package 'ht)
-(straight-use-package 'noflet)
-(straight-use-package 'memoize)
-(straight-use-package 'general)
-(straight-use-package 'posframe)
-(straight-use-package 'el-patch)
-(straight-use-package 'tree-sitter)
-(straight-use-package 'tree-sitter-langs)
-
-;; Must set before loading `use-package'.
-;; Don't use `use-package-always-ensure' with straight!
-(eval-when-compile
-  (with-no-warnings
-    (setq use-package-verbose t)))
-
-(straight-use-package 'bind-map)
-(straight-use-package 'use-package)
-
-;; To avoid an obsolete version of Org get loaded.
-(straight-use-package 'org)
-(straight-use-package 'org-contrib)
-
-(eval-when-compile
-  (require 'use-package))
-
-
-;; Setup Emacs paths
-
-(require 'zc-paths
-         (expand-file-name "config/zc-paths.el" user-emacs-directory))
-(zc-paths/init-load-paths)
-(zc-paths/init-local-modules)
-
-(require 'zc-funcs)
-(require 'zc-secret-funcs)
-
-
-;; Load features
-
-(use-package zc-theme)
-(use-package zc-core)
-(use-package zc-evil)
-;; (use-package zc-ivy) ;; replaced with zc-completion
-(use-package zc-completion)
-(use-package zc-hydra)
-(use-package zc-osx :if (eq system-type 'darwin))
-(use-package zc-eval)
-(use-package zc-editing)
-(use-package zc-eshell)
-(use-package zc-term)
-(use-package zc-git)
-(use-package zc-projectile)
-(use-package zc-modeline)
-(use-package zc-layout)
-(use-package zc-smartparens)
-(use-package zc-flycheck)
-(use-package zc-flyspell)
-(use-package zc-company)
-(use-package zc-lsp)
-(use-package zc-lisp)
-(use-package zc-tools)
-(use-package zc-treemacs)
-(use-package zc-yasnippet)
-(use-package zc-cc)
-(use-package zc-sql)
-(use-package zc-org)
-(use-package zc-web)
-(use-package zc-lua)
-(use-package zc-yaml)
-(use-package zc-rust)
-(use-package zc-scala)
-(use-package zc-swift)
-(use-package zc-kotlin)
-(use-package zc-python)
-(use-package zc-markdown)
-(use-package zc-restclient)
-(use-package zc-typescript)
-(use-package zc-plantuml)
-(use-package zc-mermaid)
-(use-package zc-prodigy)
-(use-package zc-prisma)
-(use-package zc-help)
-
-
-;; Print overall startup time.
-
-(unless noninteractive
-  (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
-    (message "Loading %s...done (%.3fs)" load-file-name elapsed))
-
-  (add-hook 'after-init-hook
-            `(lambda ()
-               (let ((elapsed (float-time (time-subtract (current-time)
-                                                         emacs-start-time))))
-                 (message ">>> Loading %s...done (%.3fs) [after-init]"
-                          ,load-file-name elapsed)))
-            t))
-
-
-
-(provide 'init)
+       :config
+       ;;literate
+       (default +bindings +smartparens))
---
+++ legacy/init.el
@@ -0,0 +1,178 @@
+;;; init.el --- Startup file for Emacs.  -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2018 Frederick Cai
+;;
+;; Author: Frederick Cai <frederick.cai@gmail.com>
+;;
+;;; Commentary:
+
+;; This emacs setting is adapted from Chris Barrett's config here:
+;; https://github.com/chrisbarrett/.emacs.d
+
+;; Install libgccjit if necessary:
+;;
+;;   brew reinstall gcc libgccjit
+;;
+;; Install Emacs on MacOS:
+;; https://github.com/railwaycat/homebrew-emacsmacport
+;;
+;;   brew install emacs-mac --with-emacs-sexy-icon
+;;
+;; https://github.com/d12frosted/homebrew-emacs-plus
+;;
+;;   brew install emacs-plus --with-modern-sexy-v2-icon
+
+(when (version< emacs-version "26")
+  (error "This requires Emacs 26 and above!"))
+
+(defconst emacs-start-time (current-time))
+
+(unless noninteractive
+  (message "Loading %s..." load-file-name))
+
+
+
+(defun zc/increase-gc-limit ()
+  "Increase garbage collection limit to maximum value."
+  (setq gc-cons-threshold most-positive-fixnum))
+
+(defun zc/reset-gc-limit ()
+  "Restore to defalut value, 16mb."
+  (setq gc-cons-threshold (* 16 1024 1024)))
+
+;; Defer GC further back to speed up startup
+(zc/increase-gc-limit)
+(add-hook 'emacs-startup-hook #'zc/reset-gc-limit)
+
+
+;; Bootstrap straight.el package manager.
+
+(defvar bootstrap-version)
+(let ((bootstrap-version 5)
+      (bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory)))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+
+  ;; Improve bootstrap speed. The cache wile may grow quite
+  ;; large overtime, if so run `straight-prune-build'.
+  (with-no-warnings
+    (setq straight-cache-autoloads t)
+    (setq straight-check-for-modifications 'live)
+    (setq straight-disable-native-compile nil))
+
+  (load bootstrap-file nil 'nomessage))
+
+
+;; Install basic packages
+
+(straight-use-package 'dash)
+(straight-use-package 'f)
+(straight-use-package 's)
+(straight-use-package 'lv)
+(straight-use-package 'ht)
+(straight-use-package 'noflet)
+(straight-use-package 'memoize)
+(straight-use-package 'general)
+(straight-use-package 'posframe)
+(straight-use-package 'el-patch)
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+
+;; Must set before loading `use-package'.
+;; Don't use `use-package-always-ensure' with straight!
+(eval-when-compile
+  (with-no-warnings
+    (setq use-package-verbose t)))
+
+(straight-use-package 'bind-map)
+(straight-use-package 'use-package)
+
+;; To avoid an obsolete version of Org get loaded.
+(straight-use-package 'org)
+(straight-use-package 'org-contrib)
+
+(eval-when-compile
+  (require 'use-package))
+
+
+;; Setup Emacs paths
+
+(require 'zc-paths
+         (expand-file-name "config/zc-paths.el" user-emacs-directory))
+(zc-paths/init-load-paths)
+(zc-paths/init-local-modules)
+
+(require 'zc-funcs)
+(require 'zc-secret-funcs)
+
+
+;; Load features
+
+(use-package zc-theme)
+(use-package zc-core)
+(use-package zc-evil)
+;; (use-package zc-ivy) ;; replaced with zc-completion
+(use-package zc-completion)
+(use-package zc-hydra)
+(use-package zc-osx :if (eq system-type 'darwin))
+(use-package zc-eval)
+(use-package zc-editing)
+(use-package zc-eshell)
+(use-package zc-term)
+(use-package zc-git)
+(use-package zc-projectile)
+(use-package zc-modeline)
+(use-package zc-layout)
+(use-package zc-smartparens)
+(use-package zc-flycheck)
+(use-package zc-flyspell)
+(use-package zc-company)
+(use-package zc-lsp)
+(use-package zc-lisp)
+(use-package zc-tools)
+(use-package zc-treemacs)
+(use-package zc-yasnippet)
+(use-package zc-cc)
+(use-package zc-sql)
+(use-package zc-org)
+(use-package zc-web)
+(use-package zc-lua)
+(use-package zc-yaml)
+(use-package zc-rust)
+(use-package zc-scala)
+(use-package zc-swift)
+(use-package zc-kotlin)
+(use-package zc-python)
+(use-package zc-markdown)
+(use-package zc-restclient)
+(use-package zc-typescript)
+(use-package zc-plantuml)
+(use-package zc-mermaid)
+(use-package zc-prodigy)
+(use-package zc-prisma)
+(use-package zc-help)
+
+
+;; Print overall startup time.
+
+(unless noninteractive
+  (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
+    (message "Loading %s...done (%.3fs)" load-file-name elapsed))
+
+  (add-hook 'after-init-hook
+            `(lambda ()
+               (let ((elapsed (float-time (time-subtract (current-time)
+                                                         emacs-start-time))))
+                 (message ">>> Loading %s...done (%.3fs) [after-init]"
+                          ,load-file-name elapsed)))
+            t))
+
+
+
+(provide 'init)
