eda6fc62b71f817ae2e7319e13ee25e3e4cbade5
Frederick Cai <frederick.cai@mattr.global>
2024-05-12 09:38:55 +1200

    keybinds: SPC-n-i for consult-org-heading

--- modules/config/zc-keybinds/config.el
+++ modules/config/zc-keybinds/config.el
@@ -274,6 +274,7 @@
        :desc "Search babel"                    "b" (cmd! (zc-org/outline-file-heading 'babel))
        :desc "Search notes"                    "n" (cmd! (zc-org/outline-file-heading 'note))
        :desc "Search work notes"               "w" (cmd! (zc-org/outline-file-heading 'work))
+       :desc "Search heading"                  "i" #'consult-org-heading
 
        :desc "Org capture"                     "c" #'org-capture
        :desc "Toggle last org-clock"           "+" #'+org/toggle-last-clock
