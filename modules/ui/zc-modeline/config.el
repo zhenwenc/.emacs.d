6bef3fb629de725245402a3ef0770263a54a35ab
Frederick Cai <frederick.cai@gmail.com>
2024-05-30 13:42:10 +1200

    ui: fix deprecated modeline segment

--- modules/ui/zc-modeline/config.el
+++ modules/ui/zc-modeline/config.el
@@ -4,6 +4,7 @@
   (setq doom-modeline-buffer-file-name-style 'relative-to-project)
 
   ;; Define custom modeline
+  ;; https://github.com/seagle0128/doom-modeline/blob/master/doom-modeline.el
   (doom-modeline-def-modeline 'zc-main
     '(eldoc bar workspace-name window-number modals matches
       follow buffer-info remote-host buffer-position
@@ -11,7 +12,7 @@
     '(compilation objed-state misc-info persp-name battery grip
       irc mu4e gnus github debug repl lsp
       minor-modes input-method indent-info
-      buffer-encoding major-mode process vcs checker time))
+      buffer-encoding major-mode process vcs check time))
 
   (defun zc/doom-modeline-set-default-modeline-h ()
     (doom-modeline-set-modeline 'zc-main 'default)))
