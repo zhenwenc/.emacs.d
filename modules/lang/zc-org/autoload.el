784ce17b94a5137d8d0557e0ec29ccd65c64d4af
Frederick Cai <frederick.cai@mattr.global>
2024-07-09 07:30:39 +1200

    org: tmux clear history

--- modules/lang/zc-org/autoload.el
+++ modules/lang/zc-org/autoload.el
@@ -471,7 +471,8 @@ This function is called by `org-babel-execute-src-block'."
          ((s-equals? "yes" (cdr (assq :compile params)))
           (compile (format "%s %s" cmd script-file)))
          ((not (s-blank? tmux))
-          (shell-command (format "tmux send-keys %s -R C-l" tmux))
+          (shell-command (format "tmux send-keys %s -X cancel" tmux)) ;; exit copy-mode
+          (shell-command (format "tmux send-keys %s -R C-l" tmux)) ;; clear history
           (shell-command (format "tmux send-keys %s '%s %s' ENTER" tmux cmd script-file)))))
     ;; Execute the code block with `org-babel-execute'
     (funcall orig-fn body params)))
