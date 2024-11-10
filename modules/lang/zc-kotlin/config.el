e5752462e33e5b982f110c6ecc5c8884940c7dc2
Frederick Cai <frederick.cai@mattr.global>
2024-11-11 08:04:33 +1300

    kotlin: add doc comment sp pair

--- modules/lang/zc-kotlin/config.el
+++ modules/lang/zc-kotlin/config.el
@@ -2,4 +2,18 @@
 
 (use-package! kotlin-mode
   :config
-  (setq kotlin-tab-width 2))
+  (setq kotlin-tab-width 2)
+
+  (after! smartparens
+    ;; Enter > right before the slash in a self-closing tag automatically
+    ;; inserts a closing tag and places point inside the element
+    (map! :map typescript-mode-map :i ">" #'zc-typescript/sp-jsx-rewrap-tag)
+
+    ;; Expand C-style comment blocks. Doom is missing the `kotlin-mode'.
+    (sp-with-modes '(kotlin-mode)
+      (sp-local-pair "/*" "*/"
+                     :actions '(insert)
+                     :post-handlers '(("| " "SPC")
+                                      (" | " "*")
+                                      ("|[i]\n[i]" "RET")))))
+  )
