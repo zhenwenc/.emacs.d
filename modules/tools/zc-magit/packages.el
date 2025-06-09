9a569c2d17003748554917b0ede7ea1edbced262
Frederick Cai <frederick.cai@mattr.global>
2025-06-10 10:31:05 +1200

    git: unpin transient

--- modules/tools/zc-magit/packages.el
+++ modules/tools/zc-magit/packages.el
@@ -4,4 +4,4 @@
 (package! git-commit-jira-prefix
   :recipe (:host github :repo "chrisbarrett/git-commit-jira-prefix"))
 
-(package! transient :pin "000ff15942878aa1108abaa020da86ada675fea9") ; 0.8.3 2025-01-04
+;; (package! transient :pin "000ff15942878aa1108abaa020da86ada675fea9") ; 0.8.3 2025-01-04
