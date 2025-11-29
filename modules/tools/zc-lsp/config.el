56125321b64dd32d3cae474ad5c9990022897a00
Frederick Cai <fred.cai@tvnz.co.nz>
2025-11-30 09:00:32 +1300

    lsp: ask eglot to stay out of json-mode

--- modules/tools/zc-lsp/config.el
+++ modules/tools/zc-lsp/config.el
@@ -3,4 +3,10 @@
 (after! eglot
   ;; Doom uses `eldoc-hint' by default, which look quite odd in the eldoc area.
   ;; The `mode-line' option actually makes it hidden.
-  (setq eglot-code-action-indications '(mode-line)))
+  (setq eglot-code-action-indications '(mode-line))
+
+  ;; Leave some major modes to their standard module.
+  (defun zc-lsp/maybe-disable-eglot ()
+    (when (eq major-mode 'json-mode)
+      (setq eglot--managed-mode nil)))
+  (add-hook 'eglot-managed-mode-hook #'zc-lsp/maybe-disable-eglot))
