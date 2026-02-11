97df6575a0d6131b75d7932fa389c7bd7d9b4fdb
Frederick Cai <frederick.cai@gmail.com>
2026-02-11 14:53:24 +1300

    chore(tools): fix docker compose

--- modules/tools/zc-docker/config.el
+++ modules/tools/zc-docker/config.el
@@ -1,7 +1,12 @@
 ;;; tools/zc-docker/config.el -*- lexical-binding: t; -*-
 
 (with-eval-after-load 'docker-process
-  (setq docker-run-async-with-buffer-function 'docker-run-async))
+  ;; FIXME This option has been deprecated since 2.5.0
+  ;;       How to run docker compose commands asynchronously now?
+  ;; (setq docker-run-async-with-buffer-function 'docker-run-async)
+  (setq docker-run-async-with-buffer-function nil)
+
+  (setq docker-terminal-backend 'auto))
 
 (with-eval-after-load 'docker-compose
   ;; Use compose V2 command
