162bc87ecf50162c6a7b736487ebd825fa098a03
Frederick Cai <frederick.cai@gmail.com>
2026-04-22 20:09:10 +1200

    fix: disable so-long-mode

--- modules/core/zc-prelude/config.el
+++ modules/core/zc-prelude/config.el
@@ -2,3 +2,9 @@
 
 (use-package! ht)
 
+;; HACK Doom's black magic is problematic!
+;;
+;; Disable `so-long' entirely. Doom enables `global-so-long-mode' on
+;; `doom-first-file-hook' in `lisp/doom-editor.el'.
+(remove-hook 'doom-first-file-hook #'global-so-long-mode)
+(after! so-long (global-so-long-mode -1))
